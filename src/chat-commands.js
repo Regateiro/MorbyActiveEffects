f52b2fffb8a371df5ec648d10b8aa17ab5df32d1
Diogo Regateiro <diogoregateiro@ua.pt>
2023-04-23 17:17:56 +0100

    Add comments.

--- src/chat-commands.js
+++ src/chat-commands.js
@@ -1,6 +1,9 @@
 import { EFFECT_MODE } from "./effects.js";
 import { controlledTokens, effectsAPI } from "./morby-active-effects.js";
 
+/**
+ * Internal effect information
+ */
 const _EFFECT_INFO = {
     "bless": {
         name: "Bless",
@@ -61,6 +64,9 @@ const _EFFECT_INFO = {
     }
 };
 
+/**
+ * Effect aliases to information mapping
+ */
 const EFFECTS = {
     "bless": _EFFECT_INFO["bless"],
     "goa": _EFFECT_INFO["giftofalacrity"],
@@ -74,6 +80,10 @@ const EFFECTS = {
     "lacerated": _EFFECT_INFO["lacerated"],
 };
 
+/**
+ * Register the mae command with Foundry
+ * @param {*} commands
+ */
 export function cm_register(commands) {
     commands.register({
         name: "/mae",
@@ -87,61 +97,106 @@ export function cm_register(commands) {
     });
 };
 
+/**
+ * Handle a chat command
+ * @param {*} chat
+ * @param {*} parameters
+ * @param {*} messageData
+ * @returns
+ */
 async function handleCommand(chat, parameters, messageData) {
+    // Split the parameters into separate fields
     parameters = parameters.toLowerCase().split(" ");
+
+    // If the first parameter corresponds to an effect
     if(Object.keys(EFFECTS).includes(parameters[0])) {
+        // Get the effect information
         const effectInfo = EFFECTS[parameters[0]];
+        // Determine if the effect has a default value or not, and if not, assert that one is provided
         if (Boolean(effectInfo.default) || Boolean(parameters[1])) {
+            // For each token that is selected
             for (const token of Object.values(controlledTokens)) {
+                // Remove any effect with the same name
                 await effectsAPI.removeEffectOnToken(token.id, effectInfo.name);
+                // Create a new effect
                 const effect = await effectsAPI.buildDefault(null, effectInfo.name, effectInfo.icon);
                 effect.isTemporary = true;
                 effect.seconds = effectInfo.seconds;
                 effect.turns = null;
                 effect.rounds = null;
-                effect.changes.length = 0;
                 for (const change of effectInfo.changes) {
                     effect.changes.push({key: change, value: parameters[1] || effectInfo.default, mode: EFFECT_MODE.ADD});
                 }
+                // Add the effect to the token
                 await effectsAPI.addEffectOnToken(token.id, effectInfo.name, effect);
             }
         }
+    // If the first parameter is a clear command
     } else if (parameters[0] == "clear") {
+        // Determine if the second parameter corresponds to an effect
         if(Object.keys(EFFECTS).includes(parameters[1])) {
+            // Get the effect information
             const effectInfo = EFFECTS[parameters[1]];
+            // For each token that is selected
             for (const token of Object.values(controlledTokens)) {
+                // Remove any effect with the same name
                 await effectsAPI.removeEffectOnToken(token.id, effectInfo.name);
             }
         }
     }
+
     return {};
 };
 
+/**
+ * Handle requests for chat autocomplete
+ * @param {*} menu
+ * @param {*} alias
+ * @param {*} parameters
+ * @returns
+ */
 function handleAutoComplete(menu, alias, parameters) {
+    // Split the parameters into separate fields
     parameters = parameters.toLowerCase().split(" ");
+
+    // Create an empty list of autocomplete entries
     let entries = [];
+
+    // If the first parameter is being written
     if(parameters.length == 1) {
+        // Add all the effect entries that match the start of the first parameter
         Object.keys(EFFECTS).filter(effect => effect.startsWith(parameters[0])).forEach(effect => {
             entries.push(game.chatCommands.createInfoElement(EFFECTS[effect].commands));
         });
+        // If the first parameter is matching the start of the clear command
         if("clear".startsWith(parameters[0])) {
+            // Add a seperator if the entry list is not empty
             if(entries.length > 0) entries.push(game.chatCommands.createSeparatorElement());
+            // Add clear as a possible command to the autocompletion
             entries.push(game.chatCommands.createInfoElement("clear"));
         }
+    // if the second parameter is being written
     } else if(parameters.length == 2) {
         switch(parameters[0]) {
+            // If the first parameter is a clear command
             case "clear":
+                // Add all the effect entries that match the start of the second parameter
                 Object.keys(EFFECTS).filter(effect => effect.startsWith(parameters[1])).forEach(effect => {
                     entries.push(game.chatCommands.createInfoElement(EFFECTS[effect].commands));
                 });
                 break;
             default:
+                // Otherwise, add the help description of the effect specified in the first parameter
                 if(Object.keys(EFFECTS).includes(parameters[0])) {
                     entries.push(game.chatCommands.createInfoElement(EFFECTS[parameters[0]].help));
                 }
                 break;
         }
     }
+
+    // Trim the entry list
     entries.length = Math.min(entries.length, menu.maxEntries);
+
+    // Return the autocomplete entries
     return entries;
 };
