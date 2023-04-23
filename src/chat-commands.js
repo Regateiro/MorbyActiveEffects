7ef03f4540dfcc6da917c6f80e212fb2854cad61
Diogo Regateiro <diogoregateiro@ua.pt>
2023-04-23 11:59:47 +0100

    Add other supported effects and conditions.

--- src/chat-commands.js
+++ src/chat-commands.js
@@ -15,16 +15,34 @@ export function cm_register(commands) {
 
 async function handleCommand(chat, parameters, messageData) {
     parameters = parameters.toLowerCase().split(" ");
-    if (parameters[0] == "lacerated") {
-        const lacerated = await effectsAPI.buildDefault(null, "Lacerated", "icons/skills/wounds/injury-triple-slash-bleed.webp");
-        lacerated.isTemporary = Boolean(60);
-        lacerated.seconds = 60;
-        lacerated.turns = null;
-        lacerated.rounds = null;
-        lacerated.changes.push({key: "flags.mae.lacerated", value: parameters[1], mode: EFFECT_MODE.ADD});
-        await effectsAPI.addEffectOnToken(controlledToken.id, "Lacerated", lacerated);
-        return {}; // Do nothing.
-    } else {
-        return; // Use core handling (this would fail because siege is not a core command).
+    switch(parameters[0]) {
+        case "lacerated":
+            const lacerated = await effectsAPI.buildDefault(null, "Lacerated", "icons/skills/wounds/injury-triple-slash-bleed.webp");
+            lacerated.isTemporary = true;
+            lacerated.seconds = 60;
+            lacerated.turns = null;
+            lacerated.rounds = null;
+            lacerated.changes.push({key: "flags.mae.lacerated", value: parameters[1], mode: EFFECT_MODE.ADD});
+            await effectsAPI.addEffectOnToken(controlledToken.id, "Lacerated", lacerated);
+            break;
+        case "heroism":
+            const heroism = await effectsAPI.buildDefault(null, "Heroism", "icons/magic/life/heart-cross-strong-blue.webp");
+            lacerated.isTemporary = true;
+            lacerated.seconds = 60;
+            lacerated.turns = null;
+            lacerated.rounds = null;
+            lacerated.changes.push({key: "flags.mae.heroismTempHP", value: parameters[1], mode: EFFECT_MODE.ADD});
+            await effectsAPI.addEffectOnToken(controlledToken.id, "Heroism", heroism);
+            break;
+        case "initiativeBonus":
+            const initiativeBonus = await effectsAPI.buildDefault(null, "Initiative Bonus", "icons/skills/movement/arrows-up-trio-red.webp");
+            lacerated.isTemporary = true;
+            lacerated.seconds = 28800;
+            lacerated.turns = null;
+            lacerated.rounds = null;
+            lacerated.changes.push({key: "flags.mae.initBonus", value: parameters[1], mode: EFFECT_MODE.ADD});
+            await effectsAPI.addEffectOnToken(controlledToken.id, "Initiative Bonus", initiativeBonus);
+            break;
     }
+    return {};
 }
